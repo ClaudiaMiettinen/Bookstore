c4a498d10baab0cd1bd7be7ee487ded369067fe6
ClaudiaMiettinen <127781221+ClaudiaMiettinen@users.noreply.github.com>
2023-09-13 21:58:38 +0300

    Add files via upload
    2nd commit

--- Bookstore/src/main/java/bookstore/training/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/bookstore/training/Bookstore/BookstoreApplication.java
@@ -1,13 +1,34 @@
 package bookstore.training.Bookstore;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+import bookstore.training.Bookstore.BookstoreApplication;
+import bookstore.training.Bookstore.domain.Book;
+import bookstore.training.Bookstore.domain.BookRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
-
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+	@Bean
+	public CommandLineRunner bookDemo(BookRepository repository) {
+		return (args) -> {
+			log.info("example data");
+			repository.save(new Book("The Snowman", "Jo Nesbo", 1990, "1234567-89", 30.00));
+			repository.save(new Book("Dark forces", "Stephen Leather", 1999, "9876543-21", 25.00));	
+			
+			log.info("fetch all books");
+			for (Book book : repository.findAll()) {
+				log.info(book.toString());
+			}
 
+		};
+	}
 }
