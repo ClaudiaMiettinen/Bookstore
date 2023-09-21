7e99231dff54df07ed4fca0f98cb29dd565687d2
ClaudiaMiettinen <127781221+ClaudiaMiettinen@users.noreply.github.com>
2023-09-21 03:22:19 +0300

    Add files via upload
    Added some repository changes

--- Bookstore/src/main/java/bookstore/training/Bookstore/BookstoreApplication.java
+++ Bookstore/src/main/java/bookstore/training/Bookstore/BookstoreApplication.java
@@ -10,6 +10,8 @@ import org.springframework.context.annotation.Bean;
 import bookstore.training.Bookstore.BookstoreApplication;
 import bookstore.training.Bookstore.domain.Book;
 import bookstore.training.Bookstore.domain.BookRepository;
+import bookstore.training.Bookstore.domain.Category;
+import bookstore.training.Bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,11 +20,18 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner bookDemo(BookRepository repository) {
+	public CommandLineRunner bookDemo(BookRepository repository,CategoryRepository C_repository) {
 		return (args) -> {
 			log.info("example data");
-			repository.save(new Book("The Snowman", "Jo Nesbo", 1990, "1234567-89", 30.00));
-			repository.save(new Book("Dark forces", "Stephen Leather", 1999, "9876543-21", 25.00));	
+			
+			C_repository.save(new Category("Drama"));
+			C_repository.save(new Category("Thriller"));
+			C_repository.save(new Category("Romance"));
+			C_repository.save(new Category("Action"));
+			C_repository.save(new Category("Adventure"));
+			
+			repository.save(new Book("The Snowman", "Jo Nesbo", 1990, "1234567-89", 30.00, C_repository.findByName("Thriller").get(0)));
+			repository.save(new Book("Dark forces", "Stephen Leather", 1999, "9876543-21", 25.00, C_repository.findByName("Thriller").get(0)));
 			
 			log.info("fetch all books");
 			for (Book book : repository.findAll()) {
--- Bookstore/src/main/java/bookstore/training/Bookstore/web/BookController.java
+++ Bookstore/src/main/java/bookstore/training/Bookstore/web/BookController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import bookstore.training.Bookstore.domain.Book;
 import bookstore.training.Bookstore.domain.BookRepository;
+import bookstore.training.Bookstore.domain.CategoryRepository;
 
 @Controller
 public class BookController {
@@ -17,6 +18,10 @@ public class BookController {
 	@Autowired
 	private BookRepository repository;
 	//hakee local host/booklistista (url) hakee repositorysta kaikki kirjat nähtäville mitä sinne on tallennettu
+	
+	@Autowired
+	private CategoryRepository C_repository;
+	
 	@GetMapping("/booklist") 
 	public String bookList(Model model) {
 		model.addAttribute("books", repository.findAll());
@@ -27,6 +32,7 @@ public class BookController {
 	@RequestMapping(value = "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
+		model.addAttribute("category", C_repository.findAll());
 		return "addbook";
 	}
 
@@ -45,6 +51,7 @@ public class BookController {
 	@RequestMapping(value = "/edit/{id}")
 	public String showModStu(@PathVariable("id") Long bookId, Model model) {
 		model.addAttribute("book", repository.findById(bookId));
+		model.addAttribute("category", C_repository.findAll());
 	return "editbook";
 	}
 
